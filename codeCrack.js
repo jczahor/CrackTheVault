ff6526a3fbf6d2e6cae258ada25ae9c2b2704850
jczahor <jennifer.czahor@gmail.com>
2025-04-09 08:28:56 -0500

    Add files via upload

---
+++ codeCrack.js
@@ -0,0 +1,36 @@
+//Cracking the code step by step breakdown
+//Create a string for the user saying: You have received this message because you have been chosen to open an important vault. Here is the secret combination...
+//Assign three variables. Each variable contains the corresponding result of calculation using a unique arithmetic operator(+-*/). Each must equal one of the three codes in the combination.
+//Add comments throughout the code to explain each step in the process.
+//Create a dialog box displaying the vault codes and the text if using HTML and a script tag, or create a popup dialog.
+//Please submit either a file with your code or a screenshot of the code, with the pseudocode showing, or a GitHub link.
+
+
+//STEPS TO COMPLETE
+//1. Code string for user saying YOU HAVE RECIEVED THIS MESSAGE,and consent.
+//2. Create variable one with arithmetic to make 10
+//4. Create variable two with arithmetic to make 40
+//5. Create variable three with arithmetic to make 39
+//6. Alert user to secret combination via variables
+
+
+//We want the three variables coded, and then displayed to the user the result via a prompt 
+
+
+//Step 1 
+const chosenOne = prompt("You have recieved this message because you have been chosen to open an important vault. Here is the secret combination... Agree?" ) 
+
+
+//Step 2
+const combo1 = (11-1)
+
+//Step 3
+const combo2 = (5*8)
+
+//Step 4
+const combo3 = (78/2)
+
+//Step 5
+const combos = prompt(combo1 combo2 combo3)
+
+
